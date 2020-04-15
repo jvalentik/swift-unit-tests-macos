62dd3a1aaf0628436b3c1f5e6dccee6fdffd8690
Jan Valentik <jvalentik@sk.ibm.com>
2020-04-15 14:33:30 +0200

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Jan Valentik on 15/04/2020.
+//  Copyright © 2020 IBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
